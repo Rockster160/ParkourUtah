b6d6ed834e859e943b725f5f9357ea65ca424445
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-02-01 22:28:33 -0700

    Attempting to add Javascripts to Heroku.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module ParkourUtah
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += ['', '']
+    config.serve_static_assets = true
+    config.assets.initialize_on_precompile false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
