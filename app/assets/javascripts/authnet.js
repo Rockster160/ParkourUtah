e192fdec6e03dbaf94026e411d223b5d261d7752
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-07 22:33:14 -0700

    Disabled the Authorize.Net manage payments when we are unable to connect to their services.

--- app/assets/javascripts/authnet.js
+++ app/assets/javascripts/authnet.js
@@ -1,7 +1,12 @@
 var ready = function() {
   if ($('.authnet-token').val() == "") {
     $('.authnet-btn').val('Sorry, Authorize.Net is not available at this time.');
+    $('.authnet-btn').addClass('disabled');
   }
+
+  $('.authnet-btn').click(function(e) {
+    e.preventDefault();
+  })
 }
 
 $(document).ready(ready);
