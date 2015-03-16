5bdee56c3f578ccd4aef2490511a015620c40372
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-16 12:14:22 -0600

    Created workers to randomly assign attendences.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 every 1.day, at: '2:00 pm' do
-  runner "Event.first.send_text"
+  runner "Scheduled.first.send_text"
 end
 
 every 1.day, at: "9:10 pm" do
-  runner "Event.first.send_summary"
+  runner "Scheduled.first.send_summary"
 end
 
 every :saturday, at: "9:30 pm" do
-  runner "Event.first.send_weekly_summary"
+  runner "Scheduled.first.send_weekly_summary"
 end
