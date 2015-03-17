1d0fa3ff66a75e6d0017e40231cd3032276db2ad
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-17 08:39:05 -0600

    Made the phone formatting a little more friendly.

--- app/assets/javascripts/text_manipulation.js
+++ app/assets/javascripts/text_manipulation.js
@@ -8,15 +8,20 @@ var ready = function() {
     }
   });
 
-  $('.formatPhoneNumber').keypress(function(e) {
-    var value = $(this).val();
-    var add = "";
-    // TODO This should sanitize the input and re-input the special characters.
-    if (value.length == 0) { add = "(" }
-    if (value.length == 4) { add = ") " }
-    if (value.length == 9) { add = "-" }
-    if (value.length > 12) { value = value.substring(0, 13) }
-    $(this).val(value + add);
+  $('.formatPhoneNumber').keyup(function(e) {
+    if (!(e.which >= 48 && e.which <= 57)) { e.preventDefault();return}
+    var value = $(this).val().replace( /\D+/g, '');
+    if (value.length >= 0) {
+      value = "("  + value
+    }
+    if (value.length > 4) {
+      value = value.substr(0, 4) + ") "  + value.substr(4)
+    }
+    if (value.length > 9) {
+      value = value.substr(0, 9) + "-"  + value.substr(9)
+    }
+    if (value.length > 13) { value = value.substring(0, 14) }
+    $(this).val(value);
   });
 
   $('.pin-entry').keypress(function(e) {
