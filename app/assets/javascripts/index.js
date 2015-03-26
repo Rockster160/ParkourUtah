5a60aed37af172c3afb8ea791044b42b892dc6c8
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-26 17:24:13 -0600

    Fixed the navbar being funky.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,20 +5,25 @@ var ready = function() {
     /* smooth scrolling for scroll to top */
     $('.scroll-link').click(function(e){
       e.preventDefault();
-      $href = e.target.hash
-      $('body,html').animate({scrollTop:$($href).offset().top},500);
-      console.log($($href).offset().top);
+      $href = $(e.target.hash).offset().top;
+      $('body,html').animate({scrollTop:$($href)},500);
     });
     /* Remove buffer for Navbar. */
     $('.navbar-offset').hide();
     /* highlight the top nav as scrolling occurs */
     $('body').scrollspy({ target: '#navbar' });
-    console.log($('.header-container').height());
-    $('.navbar-fixed-top').affix({
-      offset: {
-        top: -5
+
+    $('body').scroll(function() {
+      if (inViewport($('.header-container'))) {
+        if ($('.navbar-fixed-top').hasClass('affix')) {
+          $('.navbar-fixed-top').removeClass('affix');
+        }
+      } else {
+        if (!($('.navbar-fixed-top').hasClass('affix'))) {
+          $('.navbar-fixed-top').addClass('affix');
+        }
       }
-    });
+    })
     $('.callout-container').ready(function() {
       $('.callout-background').append(
         $('.callout-container').css('visibility', 'visible').show()
@@ -27,16 +32,25 @@ var ready = function() {
 
   } else {
 
-    $('body').scrollspy({ target: '#navbar' });
-    $('.navbar-fixed-top').affix({
-      offset: {
-        top: -10
-      }
-    });
+    if (!($('.navbar-fixed-top').hasClass())) {
+      $('.navbar-fixed-top').addClass('affix');
+    }
     $('.full-site-container').css({
       "margin": "0 auto",
       "max-width": "1200px"
     });
+  }
+
+  function inViewport($ele) {
+    var lBound = $(window).scrollTop(),
+        uBound = lBound + $(window).height(),
+        top = $ele.offset().top,
+        bottom = top + $ele.outerHeight(true);
+
+    return (top > lBound && top < uBound)
+        || (bottom > lBound && bottom < uBound)
+        || (lBound >= top && lBound <= bottom)
+        || (uBound >= top && uBound <= bottom);
   }
 };
 
