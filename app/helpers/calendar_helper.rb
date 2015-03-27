6bb755a92ba71d38b1b7611d896452484bcea450
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-26 20:21:20 -0600

    Refactored the calendar to be mobile friendly.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -2,4 +2,8 @@ module CalendarHelper
   def calendar(date = Date.today, &block)
     Calendar.new(self, date, block).table
   end
+
+  def mobile_calendar(date = Date.today, &block)
+    MobileCalendar.new(self, date, block).table
+  end
 end
