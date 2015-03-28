7c49bdd36236e4fbb66bae286f925729b61695aa
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-03-28 14:53:49 -0600

    Creating a waiver now pops up with a modal wo once again verify that all the terms are agreed to.

--- app/models/waiver.rb
+++ app/models/waiver.rb
@@ -10,18 +10,23 @@ class Waiver < ActiveRecord::Base
 
   belongs_to :dependent
   validate :has_matching_name_as_athlete
+  after_create :signed_false
 
   def exp_date
     (self.created_at + 1.year - 1.day).strftime('%B %-d, %Y')
   end
 
   def is_active?
-    true unless Date.today > self.exp_date
+    true unless Date.today > self.exp_date && self.signed
   end
 
   def has_matching_name_as_athlete
     unless Dependent.find(self.dependent_id).full_name == self.signed_for
       errors.add(:signed_for, "Athlete name must match the one listed.")
     end
+  end
+
+  def signed_false
+    self.signed = false
   end
 end
