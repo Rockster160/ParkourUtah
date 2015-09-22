5e8a9066eaa306c53febde145d9b31aefcd49825
Rocco <rocco11nicholls@gmail.com>
2015-09-22 09:41:44 -0600

    Updated the Time Zone.

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module ParkourUtah
     config.eager_load_paths += %W(#{config.root}/app/workers)
     config.exceptions_app = self.routes
 
+    config.time_zone = 'Mountain Time (US & Canada)'
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
