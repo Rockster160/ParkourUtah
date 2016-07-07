f45b3c056384de16da67fccf75f68cd474d2b8b3
Rocco <rocco11nicholls@gmail.com>
2016-07-06 21:57:03 -0600

    Updates to mailer

--- app/workers/send_temp_mail_to_all_users_worker.rb
+++ app/workers/send_temp_mail_to_all_users_worker.rb
@@ -1,9 +1,12 @@
 class SendTempMailToAllUsersWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform
+    count = User.count
     User.all.each do |u|
-      WelcomeMailer.temp_mail(u.email).deliver_now
+      WelcomeMailer.delay.temp_mail(u.email).deliver
+      puts "Emailed: #{u.email}. #{count -= 1} users to go."
       sleep 5
     end
   end
