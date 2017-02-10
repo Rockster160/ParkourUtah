7ddbfc5e4a70500a09f2474a349d3c0c827563b5
Rocco <rocco11nicholls@gmail.com>
2017-02-09 23:27:20 -0700

    Only send single day in daily summary

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -15,7 +15,7 @@ Sidekiq.configure_client { |c| c.redis = config }
 # Sidekiq::Cron::Job.destroy_all!
 Sidekiq::Cron::Job.create(name: "post_to_custom_logger", cron: "*/5 * * * *", class: "ScheduleWorker", args: {post_to_custom_logger: nil})
 Sidekiq::Cron::Job.create(name: "send_class_text", cron: "0 * * * *", class: "ScheduleWorker", args: {send_class_text: nil})
-Sidekiq::Cron::Job.create(name: "send_summary_daily", cron: "10 21 * * *", class: "ScheduleWorker", args: {send_summary: {start_date_days_ago: 1}})
+Sidekiq::Cron::Job.create(name: "send_summary_daily", cron: "10 21 * * *", class: "ScheduleWorker", args: {send_summary: {start_date_days_ago: 0}})
 Sidekiq::Cron::Job.create(name: "waiver_checks", cron: "30 9 * * *", class: "ScheduleWorker", args: {waiver_checks: nil})
 Sidekiq::Cron::Job.create(name: "remind_recurring_payments", cron: "30 10 * * *", class: "ScheduleWorker", args: {remind_recurring_payments: nil})
 Sidekiq::Cron::Job.create(name: "monthly_subscription_charges", cron: "0 7 * * *", class: "ScheduleWorker", args: {monthly_subscription_charges: nil})
