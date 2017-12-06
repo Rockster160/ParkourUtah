8bde2a3d5fb56c685caeb448d148dc57b290cfaf
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-12-06 10:41:32 -0700

    Update Sidekiq file to load jobs from array

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,12 +12,14 @@ Sidekiq.configure_server { |c| c.redis = config }
 Sidekiq.configure_client { |c| c.redis = config }
 
 # To reset Queues in case any changed:
-# Sidekiq::Cron::Job.destroy_all!
-Sidekiq::Cron::Job.create(name: "post_to_custom_logger", cron: "*/5 * * * *", class: "ScheduleWorker", args: {post_to_custom_logger: nil})
-Sidekiq::Cron::Job.create(name: "send_class_text", cron: "*/30 * * * *", class: "ScheduleWorker", args: {send_class_text: nil})
-Sidekiq::Cron::Job.create(name: "send_summary_daily", cron: "10 21 * * *", class: "ScheduleWorker", args: {send_summary: {scope: 'day'}})
-Sidekiq::Cron::Job.create(name: "waiver_checks", cron: "30 9 * * *", class: "ScheduleWorker", args: {waiver_checks: nil})
-Sidekiq::Cron::Job.create(name: "remind_recurring_payments", cron: "30 10 * * *", class: "ScheduleWorker", args: {remind_recurring_payments: nil})
-Sidekiq::Cron::Job.create(name: "monthly_subscription_charges", cron: "0 7 * * *", class: "ScheduleWorker", args: {monthly_subscription_charges: nil})
-Sidekiq::Cron::Job.create(name: "send_summary_monthly", cron: "0 7 1 * *", class: "ScheduleWorker", args: {send_summary: {scope: 'month'}})
-Sidekiq::Cron::Job.create(name: "pull_logs_from_s3", cron: "0 * * * *", class: "ScheduleWorker", args: {pull_logs_from_s3: {}})
+cron_jobs = [
+  { name: "post_to_custom_logger",        cron: "*/5 * * * *",  class: "ScheduleWorker", args: { post_to_custom_logger: nil } },
+  { name: "send_class_text",              cron: "*/30 * * * *", class: "ScheduleWorker", args: { send_class_text: nil } },
+  { name: "send_summary_daily",           cron: "10 21 * * *",  class: "ScheduleWorker", args: { send_summary: {scope: 'day'} } },
+  { name: "waiver_checks",                cron: "30 9 * * *",   class: "ScheduleWorker", args: { waiver_checks: nil } },
+  { name: "remind_recurring_payments",    cron: "30 10 * * *",  class: "ScheduleWorker", args: { remind_recurring_payments: nil } },
+  { name: "monthly_subscription_charges", cron: "0 7 * * *",    class: "ScheduleWorker", args: { monthly_subscription_charges: nil } },
+  { name: "send_summary_monthly",         cron: "0 7 1 * *",    class: "ScheduleWorker", args: { send_summary: {scope: 'month'} } },
+  { name: "pull_logs_from_s3",            cron: "0 * * * *",    class: "ScheduleWorker", args: { pull_logs_from_s3: {} } }
+]
+Sidekiq::Cron::Job.load_from_array!(cron_jobs)
