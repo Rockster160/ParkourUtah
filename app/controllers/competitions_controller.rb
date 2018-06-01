467769d11d7dfcd4f478941d3d3bb128c668fe46
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-06-01 08:36:55 -0600

    Stop lookup for non signed in userss

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -5,7 +5,7 @@ class CompetitionsController < ApplicationController
   end
 
   def show
-    redirect_to new_user_session_path, notice: "Please sign in before registering for a competition" unless user_signed_in?
+    return redirect_to new_user_session_path, notice: "Please sign in before registering for a competition" unless user_signed_in?
     @competition = Competition.current.find(params[:id])
     @competitor = @competition.competitors.new
     @eligible_athletes = current_user.athletes.where.not(id: @competition.competitors.pluck(:athlete_id))
