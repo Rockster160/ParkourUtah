998dfc9942a917e3132a8a7d057efb160f3f7c8f
Rocco Nicholls <rocco11nicholls@gmail.com>
2019-08-29 09:33:48 -0600

    Disabled comp emails for now

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -23,7 +23,7 @@ class CompetitorsController < ApplicationController
 
     if no_charge || charge.try(:status) == "succeeded"
       @competitor.update(stripe_charge_id: charge[:id]) if charge.present?
-      ApplicationMailer.registered_competitor(@competitor.id).deliver_later
+      # ApplicationMailer.registered_competitor(@competitor.id).deliver_later
       slack_message = "New *<#{competition_url(@competition)}|#{@competition.name}>* Competitor: *#{athlete.full_name}*\n#{@competitor.selected_comp}: #{@competitor.discounted_cost} #{"(#{@competitor.coupon_code})" if @competitor.coupon_code.present?}\n<#{admin_user_url(athlete.user)}|Click here to view their account.>"
       SlackNotifier.notify(slack_message, Rails.env.production? ? "#special-purchases" : "#slack-testing")
       redirect_to complete_competitor_path(@competitor), notice: "#{athlete.full_name} is enrolled in #{@competition.name}. See you there!"
@@ -37,7 +37,7 @@ class CompetitorsController < ApplicationController
   def update
     @competitor = Competitor.find(params[:id])
     @competitor.update(approved_at: DateTime.current)
-    ApplicationMailer.approved_competitor(@competitor.id).deliver_later
+    # ApplicationMailer.approved_competitor(@competitor.id).deliver_later
     redirect_to competition_path(@competitor.competition)
   end
 
